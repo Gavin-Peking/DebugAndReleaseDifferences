8230cabe7ef9decbfb7d0ce4ada60d7e7ad958c8
Gavin-Peking <gavinlsoldier@gmail.com>
2018-12-17 10:19:40 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace DebugAndReleaseDifferences
+{
+    class Program
+    {
+        // the difference between debug and release mode won't be appear if you use 'console.Write...' method in the new thread
+        static void Main(string[] args)
+        {
+
+            int Num =1;
+            var t = new Thread(() =>
+            {
+                var isSuccess = true;
+                while (Num == 1)
+                {
+                    //Thread.VolatileRead(ref Num);
+                    isSuccess = !isSuccess;    
+                }
+            });
+            t.Start();
+            Thread.Sleep(1000);
+
+            Num = 2;
+            t.Join();
+
+            Console.WriteLine("Main Thread is over!");
+            Console.ReadKey();
+        }
+    }
+}
